03747c9f02b47a9bc910b4b1d5c4147a378981ca
erikzaadi <erik.zaadi@gmail.com>
2014-09-04 12:41:00 +0300

    fix devserver

--- dev.js
+++ dev.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public_prod'));
 app.use(errorHandler({
   dumpExceptions: true,
   showStack: true
